058214f425fddc951a326bda643716dd2d06c744
Rayan Encarnacion <rayanbalbuena@gmail.com>
2025-02-25 15:56:54 -0400

    feat: Add split layout to login screen

--- resources/js/layouts/auth/auth-split-layout.tsx
+++ resources/js/layouts/auth/auth-split-layout.tsx
@@ -12,7 +12,7 @@ export default function AuthSplitLayout({ children, title, description }: AuthLa
     const { name, quote } = usePage<SharedData>().props;
 
     return (
-        <div className="relative grid h-dvh flex-col items-center justify-center px-8 sm:px-0 lg:max-w-none lg:grid-cols-2 lg:px-0">
+        <div className="relative grid h-dvh flex-col items-center justify-center px-8 sm:px-0 lg:max-w-none lg:grid-cols-3 lg:px-0">
             <div className="bg-muted relative hidden h-full flex-col p-10 text-white lg:flex dark:border-r">
                 <div className="absolute inset-0 bg-zinc-900" />
                 <Link href={route('home')} className="relative z-20 flex items-center text-lg font-medium">
@@ -28,7 +28,7 @@ export default function AuthSplitLayout({ children, title, description }: AuthLa
                     </div>
                 )}
             </div>
-            <div className="w-full lg:p-8">
+            <div className="w-full lg:col-span-2 lg:p-8">
                 <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
                     <Link href={route('home')} className="relative z-20 flex items-center justify-center lg:hidden">
                         <AppLogoIcon className="h-10 fill-current text-black sm:h-12" />
